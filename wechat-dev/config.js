1dfb54501c07068e8362eb324042f9a6cdca95c4
songziquan <377677616@qq.com>
2020-11-04 09:40:39 +0800

    11040940

--- wechat-dev/config.js
+++ wechat-dev/config.js
@@ -7,11 +7,6 @@ export default {
   REQUESTURL: 'https://game.vrupup.com/sanguo/yangyuntian/applet/wctest/public/',
   //线上cdn资源url
   ASSETSURL: 'https://img.vrupup.com/web/cdn/szq/',
-  //背景音乐配置  
-  BGMCONFIG: {
-    open: false,//是否开启背景音乐
-    musicSrc: "https://game.flyh5.cn/resources/game/wechat/szq/gaoxiao/music.mp3"//背景音乐mp3文件地址
-  },
   //每次打开小程序是否自动更新头像昵称
   UPDATEUSERINFO: false,
   //每次打开小程序是否自动检查更新最新版本
